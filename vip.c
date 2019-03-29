6c6b47f3599296f0d99e7578bf653caf3c8e586b
LeeR <LeeReix@live.com>
2019-03-29 13:22:05 +0800

    Merge with updated Step4 (#4)
    * :bug: Fine tuning DEL key
    
    * :wrench: Fine tuning DEL again..

--- vip.c
+++ vip.c
@@ -331,6 +331,9 @@ void ed_normal_process(int c) {
       break;
     case DEL_KEY:
       ed_row_delete_char(&editor.row[CURRENT_ROW], CURRENT_COL);
+      // DEL will back to normal mode and snap cursor
+      to_normal_mode();
+      // ed_process_move(ARROW_LEFT);
       break;
     case INS_KEY:
     case INSERT_MODE_KEY:
@@ -432,6 +435,8 @@ void ed_insert_process(int c) {
       break;
     case DEL_KEY:
       ed_row_delete_char(&editor.row[CURRENT_ROW], CURRENT_COL);
+      // DEL will back to normal mode and snap cursor
+      to_normal_mode();
       break;
     default:
       ed_insert_char(c);
